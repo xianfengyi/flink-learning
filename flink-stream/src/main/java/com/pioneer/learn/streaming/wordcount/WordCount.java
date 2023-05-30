4a178e16a9a8551eb579773c14faa411983ab2e7
yixianfeng <yixianfeng@bytedance.com>
2023-05-30 13:14:01 +0800

    add datasream word count example

--- flink-stream/src/main/java/com/pioneer/learn/streaming/wordcount/WordCount.java
+++ flink-stream/src/main/java/com/pioneer/learn/streaming/wordcount/WordCount.java
@@ -1,3 +1,73 @@
 package com.pioneer.learn.streaming.wordcount;
-public class wordcount {
+
+import com.pioneer.learn.streaming.wordcount.data.WordCountData;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+import javax.sql.DataSource;
+
+/**
+ * Implements the "WordCount" program that computes a simple word occurrence histogram over text
+ * files in a streaming fashion.
+ *
+ * <p>The input is a plain text file with lines separated by newline characters.
+ *
+ * <p>Usage: <code>WordCount --input &lt;path&gt; --output &lt;path&gt;</code><br>
+ * If no parameters are provided, the program is run with default data from {@link WordCountData}.
+ *
+ * <p>This example shows how to:
+ *
+ * <ul>
+ *   <li>write a simple Flink Streaming program,
+ *   <li>use tuple data types,
+ *   <li>write and use user-defined functions.
+ * </ul>
+ */
+public class WordCount {
+    public static void main(String[] args) throws Exception {
+
+        // set up the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment();
+
+        // get input data
+        DataStream<String> text = null;
+
+        // get default test text data
+        text = env.fromElements(WordCountData.WORDS);
+
+        DataStream<Tuple2<String,Integer>> counts =
+                text.flatMap(new Tokenizer())
+                        .keyBy(value->value.f0)
+                        .sum(1);
+
+        //把数据打印到控制台并且设置并行度
+        counts.print();
+
+        // execute program
+        env.execute("Streaming WordCount");
+    }
+
+    /**
+     * Implements the string tokenizer that splits sentences into words as a user-defined
+     * FlatMapFunction. The function takes a line (String) and splits it into multiple pairs in the
+     * form of "(word,1)" ({@code Tuple2<String, Integer>}).
+     */
+    public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split("\\W+");
+
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<>(token, 1));
+                }
+            }
+        }
+    }
 }
